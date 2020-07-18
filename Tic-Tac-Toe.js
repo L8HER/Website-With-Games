b05666af8be331ccbcb992a2d4bc54a7a0cfdaa5
I8HER <Victorpeng201244@gmail.com>
2020-07-17 20:24:54 -0700

    Added Button function

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,4 +1,44 @@
-function ChangeGrid(){
+function ChangeGrid1(){
 let player1='X';
-    document.getElementById("cell1").innerHTML = player1;
+document.getElementById("cell1").innerHTML = player1;
+}
+
+function ChangeGrid2(){
+    let player1='X';
+    document.getElementById("cell2").innerHTML = player1;
+}
+
+function ChangeGrid3(){
+    let player1='X';
+    document.getElementById("cell3").innerHTML = player1;
+}
+
+function ChangeGrid4(){
+    let player1='X';
+    document.getElementById("cell4").innerHTML = player1;
+}
+
+function ChangeGrid5(){
+    let player1='X';
+    document.getElementById("cell5").innerHTML = player1;
+}
+
+function ChangeGrid6(){
+    let player1='X';
+    document.getElementById("cell6").innerHTML = player1;
+}
+
+function ChangeGrid7(){
+    let player1='X';
+    document.getElementById("cell7").innerHTML = player1;
+}
+
+function ChangeGrid8(){
+    let player1='X';
+    document.getElementById("cell8").innerHTML = player1;
+}
+
+function ChangeGrid9(){
+    let player1='X';
+    document.getElementById("cell9").innerHTML = player1;
 }
